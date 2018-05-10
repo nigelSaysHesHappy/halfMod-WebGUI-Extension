88ab8d8acee5d81efe878f142e8e5a7086a05cdf
nigel <nigathan@justca.me>
2018-05-09 22:45:53 -0600

    Update build.sh

--- src/extensions/webgui/build.sh
+++ src/extensions/webgui/build.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 #g++ -std=c++11 -I ../../include/ -o nighttpsock.o -c nighttpsock.cpp -fPIC
 #g++ -std=c++11 -I ../../include/ -o webgui_server.o -c webgui_server.cpp -fPIC
 #g++ -std=c++11 -I ../../include/ -o ../../../halfMod/plugins/webgui.hmo nighttpsock.o webgui_server.o -shared webgui.cpp -lpthread -fPIC
+/usr/bin/clang++-3.8 -std=c++11 -stdlib=libc++ -o sha1.o -c sha1.cpp
+/usr/bin/clang++-3.8 -std=c++11 -stdlib=libc++ -o base64.o -c base64.cpp
 
 /usr/bin/clang++-3.8 -std=c++11 -stdlib=libc++ -I ../../include -o ../../../halfMod/extensions/webgui.hmo ../../o/halfmod.o ../../o/str_tok.o sha1.o base64.o nighttpsock.cpp webgui_server.cpp -shared webgui.cpp -fPIC
 #g++ -std=c++11 -I ../../include/ -o ../../../halfMod/plugins/webgui.hmo ../../o/halfmod.o ../../o/str_tok.o nighttpsock.cpp webgui_server.cpp -shared webgui.cpp -fPIC
